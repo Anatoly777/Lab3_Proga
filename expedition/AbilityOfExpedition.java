14e5850bf3f207ae10714c06a1b9a2500d87de23
Anatoly-LSP-LIZER <tolik.laptev.02@mail.ru>
2020-11-28 14:05:12 +0300

    Update AbilityOfExpedition.java

--- expedition/AbilityOfExpedition.java
+++ expedition/AbilityOfExpedition.java
@@ -3,15 +3,8 @@ package expedition;
 public interface AbilityOfExpedition {
     void addExperience(String e);
     String getName();
-    void supply(String about);
     void changeLocation(geographic.GeoObject GObj);
     void moveTo(geographic.GeoObject GObj);
     geographic.GeoObject getLocation();
     void useAdv(Advice ad);
-    int getMoney();
-    void setMoney(int cash);
-    int getFood();
-    void setFood(int f);
-
-
 }
