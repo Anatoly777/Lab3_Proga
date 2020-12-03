403891c439cd8ebe1b8c5bf6539973c92955a31b
Anatoly-LSP-LIZER <tolik.laptev.02@mail.ru>
2020-12-03 15:22:54 +0300

    Delete AbilityOfExpedition.java

--- expedition/AbilityOfExpedition.java
+++
@@ -1,9 +0,0 @@
-package expedition;
-
-public interface AbilityOfExpedition {
-    void addExperience(ExperienceOfPast e);
-    String getName();
-    void changeLocation(geographic.GeoObject GObj);
-    void moveTo(geographic.GeoObject GObj);
-    geographic.GeoObject getLocation();
-}
